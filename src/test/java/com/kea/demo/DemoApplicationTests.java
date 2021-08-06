18df5e837d05fdd649eafb103f76ea74331db2fd
yanzhengui <704999709@qq.com>
2021-08-06 17:43:40 +0800

    init

---
+++ src/test/java/com/kea/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kea.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
